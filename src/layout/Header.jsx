06601463b9e96d761ad22eda889cea435c4c7cf5
Abdelrahman Amr <abdelrahmanamrxi@gmail.com>
2025-03-12 14:12:47 +0200

    Font and Colors Done

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-red-400'>
-        <p>x</p>
+    <div className='bg-primary'>
+        
       
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: { 
+      colors:{
+      primary:"#00ADAD",
+      secondary:"#038096",
+      dark:"#435E6F",
+      silver:"#A6A6A6",
+      light_silver:"#D9D9D9"
+
+    },
+    fontFamily:{
+      raleway:["Raleway","sans-serif"]
+    }
+  },
   },
   plugins: [],
 }
