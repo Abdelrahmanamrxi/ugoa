aa74d4a2963f75d5489a1741dd1e85c460f424be
Mohamed Loay <elrabany1234@gmail.com>
2025-03-30 21:59:16 +0200

    some sizes and paddings adjusted

--- src/components/TeamCards.jsx
+++ src/components/TeamCards.jsx
@@ -10,7 +10,7 @@ export default function TeamCard({ name, role, image, email, linkedin }) {
         <div 
             onMouseEnter={() => setVisible(true)}  // Show icons on hover
             onMouseLeave={() => setVisible(false)} // Hide icons when not hovering
-            className="flex relative team gap-8 w-[300px] h-[350px] md:w-[350px] md:h-[360px] xl:w-[510px] xl:h-[500px] flex-col shadow-lg
+            className="flex relative team gap-8 w-[280px] h-[300px] lg:w-[300px] lg:h-[310px] xl:w-[510px] xl:h-[500px] flex-col shadow-lg
                        justify-between items-center rounded-2xl transition-all duration-500"
         >
             {/* Profile Image */}
